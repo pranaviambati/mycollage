9e8a7652e613c98e06ab0e813b19bc45b6a63384
pranaviambati <150321539+pranaviambati@users.noreply.github.com>
2024-06-24 10:57:02 +0530

    Create MyCollageApplication.java

---
+++ src/main/java/com/example/mycollage/MyCollageApplication.java
@@ -0,0 +1,12 @@
+package com.example.mycollage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCollageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCollageApplication.class, args);
+    }
+}
